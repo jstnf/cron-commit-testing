5aa071252811a98fa9e998be5a87618db9e24d3c
Justin Figueroa <justin@ucr-secure-21-10-13-132-197.wnet.ucr.edu>
2022-04-05 16:41:52 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+import os
+import time
+
+def generate_output_folder() -> None:
+  """
+  Create the output folder if it does not already exist
+  """
+  if not os.path.isdir('generated'):
+    os.mkdir('generated')
+
+def main() -> None:
+  """
+  Perform the routine
+  """
+  generate_output_folder()
+
+  current_time = round(time.time() * 1000)
+  data = { "time": current_time }
+  with open(f'generated/{current_time}.json', 'w') as f:
+    f.write(f'{json.dumps(data)}')
+
+if __name__ == '__main__':
+  main()
